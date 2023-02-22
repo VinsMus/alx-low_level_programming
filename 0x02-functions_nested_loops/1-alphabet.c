fbfb12c321f04eaf0c2f172171f231e091aff147
VinsMus <musuyuv@gmail.com>
2023-02-22 02:59:43 +0000

    l

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * *print_alphabet - paail
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+	
+	for (letter = 'a'; letter <= 'z'; letter ++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
